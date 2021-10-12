b41f9dbd54d0d02bb54aee41f7aab14307b97283
Ilya Shcherbak <shcherbakilya@gmail.com>
2021-10-12 19:52:26 +0300

    add header component

---
+++ src/view/components/Chat/index.tsx
@@ -0,0 +1,15 @@
+// Core
+import React, { FC } from 'react';
+
+// Styles
+import { Container } from './styles';
+
+type PropTypes = {}
+
+export const Chat: FC<PropTypes> = () => {
+    return (
+        <Container>
+
+        </Container>
+    );
+};
